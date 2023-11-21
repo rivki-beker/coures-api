4f96c40557073203ff083faeedbff7dac6841ebe
rivki-beker <rb0533199490@gmail.com>
2023-11-21 18:15:50 +0200

    third commit

---
+++ web-api-courses/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace web_api_courses.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int MeetingsNum { get; set; }
+        public int Age { get; set; }
+    }
+}
